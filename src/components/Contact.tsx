7087ba223cb2214f5143490fd36428143327d203
rakhmadirahman90 <79349714+rakhmadirahman90@users.noreply.github.com>
2026-02-05 23:49:11 +0800

    Updated Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -74,7 +74,7 @@ export default function Contact() {
           {/* --- GOOGLE MAPS EMBED --- */}
           <div className="w-full h-[450px] lg:h-full min-h-[450px] rounded-3xl overflow-hidden border-4 border-slate-800 shadow-2xl relative group">
             <iframe 
-              src="https://www.google.com/maps/embed?https://www.google.com/maps/place/Jl.+Andi+Makkasau+No.171,+Ujung+Lare,+Kec.+Soreang,+Kota+Parepare,+Sulawesi+Selatan+91131/@-4.008211,119.6265653,17z/data=!3m1!4b1!4m6!3m5!1s0x2d95bb1f2c5f6249:0xee8a433e727588ca!8m2!3d-4.0082164!4d119.6291402!16s%2Fg%2F11jq768k95?entry=ttu&g_ep=EgoyMDI2MDIwMS4wIKXMDSoKLDEwMDc5MjA3M0gBUAM%3D" 
+              src="https://maps.app.goo.gl/2iqvqSC7QaFLrnZy9" 
               width="100%" 
               height="100%" 
               style={{ border: 0, filter: 'grayscale(0.2) invert(0.9) contrast(1.2)' }} 

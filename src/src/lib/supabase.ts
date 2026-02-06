ea74be5db281540f4e332a4ee293f4d50e01d614
rakhmadirahman90 <79349714+rakhmadirahman90@users.noreply.github.com>
2026-02-06 23:18:00 +0800

    Updated supabase.ts

--- src/src/lib/supabase.ts
+++ src/src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://hykrsqsznmrtszmfywjz.supabase.co';
-const supabaseAnonKey = 'sb_publishable_OY3la3o6yh7i0Ry-mzPhoQ_wo6fa3u2XfR9G'; // Pastikan salin lengkap dari dashboard
+const supabaseAnonKey = 'sb_publishable_OY3la3o6yh7i0Ry-mzPhoQ_wo6fa3u2XfR9G';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
